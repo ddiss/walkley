dce52de10e13da5aa9da6f15de9aea9c5dcf57c5
David Disseldorp <ddiss@suse.de>
2022-07-01 17:52:11 +0200

    walkley: initial groundwork for host<->lkl USB passthrough
    Accept a new "-u hex-vendor-id:hex-product-id" walkley parameter.
    If a matching vendor and product USB device is found under /dev/bus/usb
    (using directly devtmpfs I/O, not via libusb), configure lkl
    configfs/functionfs parameters for passthrough using the dummy_hcd.
    
    This commit does not provide an endpoint event loop, which is still
    needed for proper passthrough device I/O.
    
    Link: https://wiki.tizen.org/USB/Linux_USB_Layers/Configfs_Composite_Gadget/Usage_eq._to_g_ffs.ko
    Signed-off-by: David Disseldorp <ddiss@suse.de>

--- walkley.c
+++ walkley.c
@@ -3,6 +3,16 @@
 #include <string.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <assert.h>
+#include <ctype.h>
+
+#include <sys/types.h>
+#include <dirent.h>
+
 #ifdef __FreeBSD__
 #include <sys/types.h>
 #endif
@@ -20,7 +30,18 @@
 #include <linux/random.h>
 #include <wireguard.h>
 
+#include <linux/limits.h>
+#include <linux/usbdevice_fs.h>
+#include <linux/usb/ch9.h>
+
 #include "vendor/cl_arg.h"
+
+#ifdef WALKLEY_DEBUG
+#define dbg(fmt, ...) \
+	fprintf(stderr, "(%s:%d) " fmt, __FILE__, __LINE__, ##__VA_ARGS__);
+#else
+#define dbg(fmt, ...)
+#endif
 
 static u_short in_cksum(const u_short *addr, register int len, u_short csum)
 {
@@ -321,6 +342,25 @@ static int wg_teardown(void)
 	return 0;
 }
 
+static int parse_usb_ids(struct cl_arg *arg, const char *val)
+{
+	int match = 0;
+	unsigned int usb_vendor = 0;
+	unsigned int usb_product = 0;
+
+	if (!val)
+		return -EINVAL;
+
+	match = sscanf(val, "0x%04x:0x%04x", &usb_vendor, &usb_product);
+	if (match == 0)
+		match = sscanf(val, "%04x:%04x", &usb_vendor, &usb_product);
+	if ((match != 2) || ((usb_vendor | usb_product) & 0xffff0000))
+		return -EINVAL;
+
+	*(uint32_t *)arg->store = (usb_vendor << 16 | usb_product);
+	return 0;
+}
+
 struct cla_args_wg {
 	int port;
 	unsigned int tun_ip;
@@ -339,6 +379,7 @@ static struct {
 	char *mnt_dev;
 	char *mnt_fs_type;
 	char *mnt_opts;
+	uint32_t usb_vendor_product;
 	struct cla_args_wg wg;
 } cla = {
 	.ip = INADDR_NONE,
@@ -364,6 +405,8 @@ struct cl_arg args[] = {
 	 &cla.mnt_fs_type, NULL, NULL},
 	{"mnt-opts", 'O', "mount options", 1, CL_ARG_STR,
 	 &cla.mnt_opts, NULL, NULL},
+	{"usb", 'u', "passthrough USB device with 0xVENDORID:0xPRODUCTID",
+	 1, CL_ARG_USER, &cla.usb_vendor_product, NULL, parse_usb_ids},
 	{"wg-port", 'p', "wireguard port", 1, CL_ARG_INT,
 	 &cla.wg.port, NULL, NULL},
 	{"wg-tun-ip", 't', "wireguard tunnel IPv4 address", 1, CL_ARG_IPV4,
@@ -413,6 +456,424 @@ static int args_validate()
 	}
 
 	return 0;
+}
+
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
+static int lkl_usb_cfg_mount(void)
+{
+	char opts_buf[64 * 1024] = { 0 };
+	struct lkl_mnt {
+		char *dev;
+		char *dir;
+		char *fstype;
+	} mnts[] = { { "proc", "/proc" , "proc" },
+		{ "configfs", "/configfs", "configfs" },
+		{ "devtmpfs", "/dev", "devtmpfs" } };
+	int ret;
+	unsigned int i;
+
+	for (i = 0; i < ARRAY_SIZE(mnts); i++) {
+		ret = lkl_sys_mkdir(mnts[i].dir, 0700);
+		if (ret && (ret != -LKL_EEXIST)) {
+			fprintf(stderr, "failed to create dir %s: %s.\n",
+				mnts[i].dir, lkl_strerror(ret));
+			return ret;
+		}
+
+		ret = lkl_sys_mount(mnts[i].dev, mnts[i].dir, mnts[i].fstype,
+				    0, opts_buf);
+		if (ret < 0) {
+			fprintf(stderr, "failed to mount %s: %s.\n",
+				mnts[i].dev, lkl_strerror(ret));
+			return ret;
+		}
+	}
+
+	return 0;
+}
+
+static int
+lkl_path_overwrite(const char *path, char *data, ssize_t data_len,
+		   bool sync)
+{
+	ssize_t written;
+	ssize_t wr_ret;
+	int fd, ret;
+
+	fd = lkl_sys_open(path, LKL_O_WRONLY | LKL_O_CREAT, 0);
+	if (fd < 0) {
+		fprintf(stderr, "lkl_sys_open %s: %s\n",
+			path, lkl_strerror(fd));
+		return fd;
+	}
+
+	for (written = 0; written < data_len; written += wr_ret) {
+		wr_ret = lkl_sys_write(fd, data + written, data_len - written);
+		if (wr_ret <= 0) {
+			fprintf(stderr, "lkl_sys_write %s: %s\n",
+				path, lkl_strerror(wr_ret));
+			ret = -EIO;
+			goto err_close;
+		}
+	}
+
+	if (sync) {
+		ret = lkl_sys_fsync(fd);
+		if (ret < 0) {
+			fprintf(stderr, "lkl_sys_fsync %s: %s\n",
+				path, lkl_strerror(ret));
+			ret = -EIO;
+			goto err_close;
+		}
+	}
+	ret = 0;
+err_close:
+	lkl_sys_close(fd);
+	return ret;
+}
+
+unsigned int lkl_usb_gadget_dirid = 0;
+
+static int
+lkl_usb_configfs_setup(const struct usb_device_descriptor *device,
+		       const struct usb_config_descriptor *config,
+		       const struct usb_interface_descriptor *uif)
+{
+	char configfs_path[LKL_PATH_MAX];
+	char configfs_data[256];
+	char *subdir_boff;
+	size_t subdir_blen;
+	unsigned int gadget_subdir = lkl_usb_gadget_dirid;
+	int ret;
+
+	ret = snprintf(configfs_path, sizeof(configfs_path),
+		       "/configfs/usb_gadget/0x%x/", gadget_subdir);
+	if (ret <= 0 || (size_t)ret >= sizeof(configfs_path))
+		return -EINVAL;
+
+	/* subdirectory paths are appended to the end */
+	subdir_boff = configfs_path + ret;
+	subdir_blen = sizeof(configfs_path) - ret;
+
+	/* FIXME should share the same gadget dir when possible */
+	lkl_usb_gadget_dirid++;
+
+	ret = lkl_sys_mkdir(configfs_path, 0700);
+	if (ret) {
+		fprintf(stderr, "failed to create dir %s: %s.\n",
+			configfs_path, lkl_strerror(ret));
+		return ret;
+	}
+
+	ret = snprintf(subdir_boff, subdir_blen, "configs/c.1");
+	if (ret <= 0 || (size_t)ret >= subdir_blen)
+		return -EINVAL;
+
+	ret = lkl_sys_mkdir(configfs_path, 0700);
+	if (ret) {
+		fprintf(stderr, "failed to create dir %s: %s.\n",
+			configfs_path, lkl_strerror(ret));
+		return ret;
+	}
+
+	ret = snprintf(subdir_boff, subdir_blen, "functions/ffs.walkley");
+	if (ret <= 0 || (size_t)ret >= subdir_blen)
+		return -EINVAL;
+
+	ret = lkl_sys_mkdir(configfs_path, 0700);
+	if (ret) {
+		fprintf(stderr, "failed to create dir %s: %s.\n",
+			configfs_path, lkl_strerror(ret));
+		return ret;
+	}
+
+
+	ret = snprintf(subdir_boff, subdir_blen, "strings/0x409");
+	if (ret <= 0 || (size_t)ret >= subdir_blen)
+		return -EINVAL;
+
+	ret = lkl_sys_mkdir(configfs_path, 0700);
+	if (ret) {
+		fprintf(stderr, "failed to create dir %s: %s.\n",
+			configfs_path, lkl_strerror(ret));
+		return ret;
+	}
+
+	ret = snprintf(subdir_boff, subdir_blen, "configs/c.1/strings/0x409");
+	if (ret <= 0 || (size_t)ret >= subdir_blen)
+		return -EINVAL;
+
+	ret = lkl_sys_mkdir(configfs_path, 0700);
+	if (ret) {
+		fprintf(stderr, "failed to create dir %s: %s.\n",
+			configfs_path, lkl_strerror(ret));
+		return ret;
+	}
+
+	ret = snprintf(subdir_boff, subdir_blen, "idProduct");
+	if (ret <= 0 || (size_t)ret >= sizeof(configfs_data))
+		return -EINVAL;
+
+	ret = snprintf(configfs_data, sizeof(configfs_data),
+		       "0x%4x", device->idProduct);
+	if (ret <= 0 || (size_t)ret >= sizeof(configfs_data))
+		return -EINVAL;
+
+	ret = lkl_path_overwrite(configfs_path, configfs_data, ret, false);
+	if (ret < 0)
+		return ret;
+
+	ret = snprintf(subdir_boff, subdir_blen, "idVendor");
+	if (ret <= 0 || (size_t)ret >= sizeof(configfs_data))
+		return -EINVAL;
+
+	ret = snprintf(configfs_data, sizeof(configfs_data),
+		       "0x%4x", device->idVendor);
+	if (ret <= 0 || (size_t)ret >= sizeof(configfs_data))
+		return -EINVAL;
+
+	ret = lkl_path_overwrite(configfs_path, configfs_data, ret, false);
+	if (ret < 0)
+		return ret;
+
+#if 0
+	ret = snprintf(subdir_boff, subdir_blen, "UDC");
+	if (ret <= 0 || (size_t)ret >= sizeof(configfs_data))
+		return -EINVAL;
+
+	ret = lkl_path_overwrite(configfs_path, "dummy_udc",
+				 sizeof("dummy_udc") - 1);
+#endif
+
+	return 0;
+}
+
+/*
+ * parse interface and mass storage eps, returning the amount of data
+ * processed.
+ */
+static ssize_t
+host_usb_interface_parse(const struct usb_device_descriptor *device,
+			 const struct usb_config_descriptor *config,
+			 unsigned char *buf,
+			 ssize_t dlen)
+{
+	struct usb_interface_descriptor *uif
+		= (struct usb_interface_descriptor *)buf;
+	struct usb_endpoint_descriptor *uep;
+	ssize_t dlen_used = 0;
+
+	assert(uif->bDescriptorType == USB_DT_INTERFACE);
+
+	if ((buf + USB_DT_INTERFACE_SIZE < buf)
+	 || (USB_DT_INTERFACE_SIZE > dlen)
+	 || (uif->bLength != USB_DT_INTERFACE_SIZE)) {
+		fprintf(stderr, "bad USB interface desc\n");
+		return -EINVAL;
+	}
+	buf += USB_DT_INTERFACE_SIZE;
+	dlen -= USB_DT_INTERFACE_SIZE;
+	dlen_used += USB_DT_INTERFACE_SIZE;
+
+	dbg("USB interface Class=%d, SubClass=%d, Protocol=%d, "
+	    "Endpoints=%d\n", uif->bInterfaceClass, uif->bInterfaceSubClass,
+	    uif->bInterfaceProtocol, uif->bNumEndpoints);
+
+	lkl_usb_configfs_setup(device, config, uif);
+
+	/* parse remaining data, which should carry endpoints */
+        while (dlen >= 2) {
+                unsigned char this_len = buf[0];
+                unsigned char type = buf[1];
+
+		if ((this_len > dlen) || (buf + this_len < buf))
+			break;
+
+		if (type == USB_DT_ENDPOINT) {
+			uep = (struct usb_endpoint_descriptor *)buf;
+			if (this_len != USB_DT_ENDPOINT_SIZE)
+				return -EINVAL;
+
+			if (uep->bEndpointAddress & USB_ENDPOINT_DIR_MASK) {
+				dbg("-> EP %zd: IN (0x%x)\n",
+				    dlen_used, uep->bEndpointAddress);
+			} else {
+				dbg("-> EP %zd: OUT (0x%x)\n",
+				    dlen_used, uep->bEndpointAddress);
+			}
+			/* TODO add to configfs */
+		} else {
+			/* e.g. USB_DT_SS[P_ISOC]_ENDPOINT_COMP */
+			dbg("-> Skipping non-endpoint type 0x%02x\n", type);
+		}
+		buf += this_len;
+		dlen -= this_len;
+		dlen_used += this_len;
+	}
+
+out_dlen:
+	return dlen_used;
+}
+
+static int host_usb_desc_read(uint32_t usb_vendor_product, const char *devname)
+{
+	ssize_t dlen;
+	unsigned char dbuf[4096];
+	unsigned char *buf = dbuf;
+	struct usb_device_descriptor *device;
+	struct usb_config_descriptor *config;
+	int dev_fd, ret;
+
+	dev_fd = open(devname, O_RDONLY);
+	if (dev_fd < 0) {
+		fprintf(stderr,
+			"skipping USB device at %s: %d\n",
+			devname, errno);
+		ret = -errno;
+		goto err_out;
+	}
+
+	dlen = read(dev_fd, dbuf, sizeof(dbuf));
+	if ((dlen < USB_DT_DEVICE_SIZE + USB_DT_CONFIG_SIZE)
+	 || (dlen > sizeof(dbuf))) {
+		fprintf(stderr, "failed to read USB device desc at %s: %d/%d\n",
+			devname, dlen, errno);
+		ret = -EIO;
+		goto err_close;
+	}
+
+	dbg("read %zd bytes data from %s\n", dlen, devname);
+
+	device = (struct usb_device_descriptor *)buf;
+	if ((device->bLength != USB_DT_DEVICE_SIZE)
+	 || (device->bDescriptorType != USB_DT_DEVICE)) {
+		fprintf(stderr, "bad USB device desc at %s\n", devname);
+		ret = -ENODEV;
+		goto err_close;
+	}
+
+	buf += USB_DT_DEVICE_SIZE;
+	dlen -= USB_DT_DEVICE_SIZE;
+
+	fprintf(stdout, "%s Vendor=0x%04x Product=0x%04x\n",
+		devname, device->idVendor, device->idProduct);
+
+	if ((device->idVendor != (usb_vendor_product >> 16))
+	 || (device->idProduct != (usb_vendor_product & 0x0000ffff))) {
+		fprintf(stdout, "-> ignored.\n");
+		ret = 0;
+		goto err_close;
+	} else {
+		fprintf(stdout, "-> matched, proceeding with passthrough.\n");
+	}
+
+	/* buf >= USB_DT_CONFIG_SIZE checked at read time */
+	config = (struct usb_config_descriptor *)buf;
+	if ((config->bLength != USB_DT_CONFIG_SIZE)
+	 || (config->bDescriptorType != USB_DT_CONFIG)) {
+		fprintf(stderr, "bad USB config desc at %s\n", devname);
+		ret = -ENODEV;
+		goto err_close;
+	}
+
+	buf += USB_DT_CONFIG_SIZE;
+	dlen -= USB_DT_CONFIG_SIZE;
+
+        while (dlen >= 2) {
+		ssize_t dlen_used;
+                unsigned char this_len = buf[0];
+                unsigned char type = buf[1];
+
+		if (this_len > dlen)
+			break;
+
+		if (type == USB_DT_INTERFACE) {
+			dlen_used = host_usb_interface_parse(device, config,
+							     buf, dlen);
+			if (dlen_used < 0) {
+				ret = dlen_used;
+				goto err_close;
+			}
+			assert(dlen_used <= dlen);
+		} else {
+			fprintf(stderr,
+				"skipping non-interface type 0x%02x\n", type);
+			dlen_used = this_len;
+		}
+		buf += dlen_used;
+		dlen -= dlen_used;
+	}
+
+	ret = 0;
+err_close:
+	close(dev_fd);
+err_out:
+	return ret;
+}
+
+static bool numeric_name(const char *n)
+{
+	const char *orig;
+
+	for (orig = n; isdigit(*n); n++);
+
+	return (*n == '\0' && n != orig);
+}
+
+void host_usb_enum(uint32_t usb_vendor_product)
+{
+	struct dirent *de;
+	const char *busbase = "/dev/bus/usb";
+	DIR *busdir = opendir(busbase);
+
+	if (busdir == NULL) {
+		fprintf(stderr, "failed to open %s: %d\n", busbase, errno);
+		return;
+	}
+
+	while ((de = readdir(busdir)) != NULL) {
+		int l, ret;
+		char busname[PATH_MAX];
+		DIR *devdir;
+
+		if (!numeric_name(de->d_name))
+			continue;
+
+		l = snprintf(busname, sizeof(busname), "%s/%s",
+			     busbase, de->d_name);
+		if (l < 0 || (size_t)l >= sizeof(busname)) {
+			fprintf(stderr, "invalid bus path %s/%s\n",
+				busbase, de->d_name);
+			continue;
+		}
+
+		dbg("checking USB bus at %s\n", busname);
+		devdir = opendir(busname);
+		if (devdir == NULL) {
+			fprintf(stderr, "failed to open %s: %d\n",
+				busname, errno);
+			continue;
+		}
+
+		while ((de = readdir(devdir)) != NULL) {
+			char devname[PATH_MAX];
+
+			if (!numeric_name(de->d_name))
+				continue;
+			l = snprintf(devname, sizeof(devname), "%s/%s",
+				     busname, de->d_name);
+			if (l < 0 || (size_t)l >= sizeof(devname)) {
+				fprintf(stderr, "invalid path %s/%s\n",
+					busname, de->d_name);
+			}
+			dbg("opening USB device at %s\n", devname);
+
+			ret = host_usb_desc_read(usb_vendor_product, devname);
+			if (ret < 0) {
+				break;
+			}
+		}
+	}
 }
 
 int main(int argc, const char **argv)
@@ -501,6 +962,12 @@ int main(int argc, const char **argv)
 				goto out_halt;
 			}
 		}
+	}
+
+	if (cla.usb_vendor_product) {
+		ret = lkl_usb_cfg_mount();
+		if (!ret)
+			host_usb_enum(cla.usb_vendor_product);
 	}
 
 	if (cla.wg.port != 0) {
